8746635f0a7780ba53562542a1f671690e52c558
Martin Selin <seleeto@gmail.com>
2025-10-02 11:43:12 +0200

    Added properties to Contact class with default empty strings

--- Contact.cs
+++ Contact.cs
@@ -1,5 +1,10 @@
-public class Kontakt
+public class Contact
 {
-
-
+    public string FirstName { get; set; } = "";   // default empty string
+    public string LastName { get; set; } = "";    // default empty string
+    public string StreetAddress { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public string City { get; set; } = "";
+    public string PhoneNumber { get; set; } = "";
+    public string Email { get; set; } = "";
 }
